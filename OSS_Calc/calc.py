dfb8cdcb8512ee6a7e7463ac55717b8993078c39
Hyowon An <gydnjs3505@gmail.com>
2025-06-05 15:20:35 +0900

    [기능추가]메모리 저장/불러오기(M+, M-, MR, MC) 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -5,20 +5,22 @@ class Calculator:
     def __init__(self, root):
         self.root = root
         self.root.title("계산기")
-        self.root.geometry("300x400")
+        self.root.geometry("320x540")
 
         self.expression = ""
+        self.memory = 0.0   # 메모리 값(float)
 
         # 입력창
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
         self.entry.pack(fill="both", ipadx=8, ipady=15, padx=10, pady=10)
 
-        # 버튼 생성
+        # 버튼 정의 (마지막줄에 메모리 버튼 4개 추가)
         buttons = [
             ['7', '8', '9', '/'],
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
+            ['M+', 'M-', 'MR', 'MC'],
             ['=']
         ]
 
@@ -42,11 +44,42 @@ class Calculator:
                 self.expression = str(eval(self.expression))
             except Exception:
                 self.expression = "에러"
+        elif char == 'M+':
+            self.memory_add()
+        elif char == 'M-':
+            self.memory_subtract()
+        elif char == 'MR':
+            self.memory_recall()
+        elif char == 'MC':
+            self.memory_clear()
         else:
             self.expression += str(char)
 
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
 
+    # ------------- 메모리 기능 구현 ----------------
+    def memory_add(self):
+        # 현재 입력값(혹은 결과값)을 메모리에 더함
+        try:
+            val = float(self.entry.get())
+            self.memory += val
+        except Exception:
+            pass    # 숫자가 아니면 무시
 
+    def memory_subtract(self):
+        # 현재 입력값(혹은 결과값)을 메모리에서 뺌
+        try:
+            val = float(self.entry.get())
+            self.memory -= val
+        except Exception:
+            pass
+    
+    def memory_recall(self):
+        # 메모리 값을 입력창에 출력
+        self.expression = str(self.memory)
+
+    def memory_clear(self):
+        # 메모리 초기화
+        self.memory = 0.0
 
