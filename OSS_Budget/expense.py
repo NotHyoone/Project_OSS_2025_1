06a1840b299224c00dce3e30f36b714642cfebff
Hyowon An <gydnjs3505@gmail.com>
2025-06-05 15:42:28 +0900

    실행파일 외, 주석 추가부분 업데이트

--- OSS_Budget/expense.py
+++ OSS_Budget/expense.py
@@ -1,10 +1,11 @@
 
 class Expense:
     def __init__(self, date, category, description, amount):
-        self.date = date
-        self.category = category
-        self.description = description
-        self.amount = amount
+        self.date = date                # 날짜 (문자열)
+        self.category = category        # 카테고리 (예 : 식비, 교통)
+        self.description = description  # 지출 설명
+        self.amount = amount            # 금액 (int)
 
     def __str__(self):
+        # 지출 내역을 보기 좋게 문자열로 반환
         return f"[{self.date}] {self.category} - {self.description}: {self.amount}원"
